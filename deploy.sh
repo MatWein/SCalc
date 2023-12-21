d989cfa905d86700963574671d89543147d62f91
matthias.weinert <matthiasweinert@web.de>
2023-12-21 22:09:08 +0100

    fixed junit 5 problems when running mvn deploy

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# use following command first to change java version to 1.8
+# use following command first to change java version to 11
 # sudo update-alternatives --config java
 
 mvn clean deploy -P deploy-official
